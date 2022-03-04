d096845506f8d3d26e17977c44b1ff23d44e1a60
Henry Larson <hxl1116@rit.edu>
2022-03-04 11:10:18 -0500

    Swapped unittest to pytest; Added `insert_member` and `update_member` func; Added `put` and `delete` func for `Member` resource; TODOs updated

--- src/api/member.py
+++ src/api/member.py
@@ -1,14 +1,30 @@
 from flask_restful import Resource
+from flask_restful.reqparse import RequestParser
 
-from db.member import fetch_all_members
+from db.member import fetch_all_members, insert_member
 
 
 class Member(Resource):
     CREATED = 201
 
+    def __init__(self):
+        self.parser = RequestParser()
+        self.parser.add_argument('last', required=True, help='Last name cannot be blank')
+        self.parser.add_argument('first', required=True, help='First name cannot be blank')
+        self.parser.add_argument('user', required=True, help='Username cannot be blank')
+        self.parser.add_argument('email', required=True, help='Email cannot be blank')
+
     @staticmethod
     def get():
         return fetch_all_members()
 
-    def post(self, last, first, user, email, user_change_date):
-        return None, self.CREATED
+    def post(self):
+        args = self.parser.parse_args()
+
+        return insert_member(**args), self.CREATED
+
+    def put(self):
+        return None, 201
+
+    def delete(self):
+        return '', 204
--- src/db/member.py
+++ src/db/member.py
@@ -1,4 +1,4 @@
-from db.utils import fetch_many
+from db.utils import fetch_many, commit
 
 
 def fetch_all_members():
@@ -6,3 +6,17 @@ def fetch_all_members():
         SELECT *
         FROM member
     """)
+
+
+# TODO: Add error handling
+def insert_member(last, first, user, email, user_change_date):
+    commit("""
+        INSERT INTO member VALUES (last_name=%s, first_name=%s, username=%s, email=%s, username_changed_date=%s)
+    """, (last, first, user, email, user_change_date))
+
+
+# TODO: Generate dynamic query
+def update_member(id, **kwargs):
+    commit("""
+        UPDATE member SET %s = %s WHERE member_id=%s
+    """, )
