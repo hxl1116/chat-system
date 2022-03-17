80fa6456a421177d3a3f51a6d9b5b3ea9d170470
Henry Larson <hxl1116@rit.edu>
2022-03-16 21:53:39 -0400

    removed `not null` password requirement from `member` table

--- res/schema.sql
+++ res/schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS member
 (
     member_id             uuid primary key default uuid_generate_v4(),
     username              varchar(255) not null,
-    password              varchar(225) not null,
+    password              varchar(225),
     last_name             varchar(255) not null,
     first_name            varchar(255) not null,
     email                 varchar(255) not null,
