bf6927f8b49589cf71058fe3d17e7155befeb3d2
Henry Larson <hxl1116@rit.edu>
2022-03-17 23:33:26 -0400

    Re-implemented communities list with channels

--- src/api/community.py
+++ src/api/community.py
@@ -12,4 +12,10 @@ class Community(Resource):
         communities = [comm.format() for comm in Community.fetch_all()]
         channels = [chan.format() for chan in Channel.fetch_all()]
 
+        for comm in communities:
+            comm['channels'] = []
+            for chan in channels:
+                if comm['community_id'] == chan['community_id']:
+                    comm['channels'] = comm['channels'] + [chan]
+
         return communities, ResCode.SUCCESS.value
